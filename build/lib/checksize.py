0041cb2a10375d33749e53d348599ada95f933df
gespel <sten.heimbrodt@hotmail.de>
2024-01-19 18:52:40 +0100

    added file count

--- build/lib/checksize.py
+++ build/lib/checksize.py
@@ -2,20 +2,17 @@ import os
 import time
 
 def get_size(start_path = '.'):
-    num_files = 0
     total_size = 0
     for dirpath, dirnames, filenames in os.walk(start_path):
         for f in filenames:
-            num_files += 1
             fp = os.path.join(dirpath, f)
             # skip if it is symbolic link
             if not os.path.islink(fp):
                 total_size += os.path.getsize(fp)
 
-    return (num_files, total_size)
+    return total_size
 
 def checkloop():
     while(1):
-        num_files, size = get_size()
-        print(f"{num_files} files -> {size/1024/1024} MB")
+        print(f"{get_size()/1024/1024} MB")
         time.sleep(1)
